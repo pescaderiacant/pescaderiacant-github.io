7763439e23498b2d7fb6da01ebda73b2bcc1ce28
GlowingYouUp <elecorreo123@outlook.es>
2024-11-30 18:11:21 +0100

    commit 1

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const menuBtn = document.getElementById('menuBtn');
+const nav = document.querySelector('nav');
+const languageDiv = document.getElementById('languageDiv');
+const languageMenu = document.getElementById('languageMenu')
+menuBtn.addEventListener('click', ()=>{
+    nav.classList.toggle('show');
+    languageMenu.classList.remove('show-language-menu')
+})
+
+// const languageInput = document.getElementById('languageInput');
+// languageInput.addEventListener('click', ()=>{
+//     if (location.pathname == '')location.pathname = 'eu/';
+//     else if (location.pathname == '/eu/index.html') location.pathname = '/index.html'
+//     else if (location.pathname == '/nuestra-historia/index.html') location.pathname = 'eu/gure-historia/index.html'     else if (location.pathname == '/eu/gure-historia/index.html') location.pathname = 'nuestra-historia/index.html'
+// })
+
+
+languageDiv.addEventListener('click', ()=>{
+    languageMenu.classList.toggle('show-language-menu')
+})
+languageMenu.addEventListener('click',()=>{
+    if (location.pathname == '/index.html')location.pathname = '/eu/index.html';
+    else if (location.pathname == '/eu/index.html') location.pathname = '/index.html'
+    else if (location.pathname == '/nuestra-historia/index.html') location.pathname = 'eu/gure-historia/index.html';
+    else if (location.pathname == '/eu/gure-historia/index.html') location.pathname = 'nuestra-historia/index.html'
+})
